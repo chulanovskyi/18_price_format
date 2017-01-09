906757065a326734b2d0131cfbab22ae9d331ece
chulanovskyi <skyrocker@mail.ru>
2017-01-09 23:30:13 +0200

    half way

--- format_price.py
+++ format_price.py
@@ -1,5 +1,16 @@
+from argparse import ArgumentParser
+from textwrap import wrap
+
+
 def format_price(price):
-    pass
+    try:
+        inverse = str(int(float(price)))[::-1]
+        return ' '.join(wrap(inverse, 3))[::-1]
+    except ValueError:
+        return 'NaN'
+
 
 if __name__ == '__main__':
-    pass
+    parser = ArgumentParser()
+    parser.add_argument('price')
+    print(format_price(parser.parse_args().price))
--- tests.py
+++ tests.py
@@ -1,1 +1,11 @@
-# TODO place your tests here
+from format_price import format_price
+import unittest
+
+
+class TestFormatPrice(unittest.TestCase):
+    def test_invalid_number(self):
+        self.assertEqual(format_price('123.123D'), 'NaN')
+
+
+if __name__ == '__main__':
+    unittest.main()
